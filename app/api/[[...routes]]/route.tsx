a956d3d283d01a4858b832af37d1527fb980d7c5
name <mahimathacker75@gmail.com>
2024-04-10 17:04:51 +0530

    mint NFT

---
+++ app/api/[[...routes]]/route.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource frog/jsx */
+
+import { Button, Frog } from 'frog'
+import { handle } from 'frog/vercel'
+import contractAbi from './contract.json'
+
+const app = new Frog({
+  basePath: '/api',
+})
+
+app.frame('/', (c) => {
+  return c.res({
+    action: '/finish',
+    image: "https://dweb.mypinata.cloud/ipfs/QmSYN7KT847Nado3fxFafYZgG6NXTMZwbaMvU9jhu5nPmJ",
+    imageAspectRatio: "1:1",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+    ]
+  })
+})
+ 
+app.frame('/finish', (c) => {
+  return c.res({
+    image: "https://dweb.mypinata.cloud/ipfs/QmUx3kQH4vR2t7mTmW3jHJgJgJGxjoBsMxt6z1fkZEHyHJ",
+    imageAspectRatio: "1:1",
+  })
+})
+ 
+app.transaction('/mint', (c: any) => {
+  return c.contract({
+    abi: contractAbi.output.abi,
+    chainId: 'eip155:84532',
+    functionName: 'mint',
+    to: '0x1a5516BeDB0CaAB297c8cecaBdFe96325469EF2d',
+  })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
